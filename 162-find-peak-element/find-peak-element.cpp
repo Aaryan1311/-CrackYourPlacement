53fd2c4a3828281636dbc4edc215826c8026c523
Aaryan1311 <100665724+Aaryan1311@users.noreply.github.com>
2025-02-06 00:25:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (65.57%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
-        int mx = INT_MIN;
-        int idx = -1;
-        for(int i=0;i<nums.size();i++){
-            if(mx <= nums[i]){
-                mx = nums[i];
-                idx = i;
+    int shiksha(int l, int r, vector<int>& nums){
+            int mid = l + (r-l)/2;
+            if(mid != 0 && mid != nums.size()-1){
+                if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
+                else{
+                    if(nums[mid] < nums[mid+1] && nums[mid] > nums[mid-1]){
+                        return shiksha(mid,r,nums);
+                    }
+                    else if(nums[mid] > nums[mid+1] && nums[mid] < nums[mid-1]){
+                        return shiksha(l,mid,nums);
+                    }
+                    else{
+                        return shiksha(l,mid,nums);
+                    }
+                }
             }
-        }
-        return idx;
+
+            else if(mid == 0 && nums[mid+1] < nums[mid]) return mid;
+            else if(mid == nums.size()-1 && nums[mid] > nums[mid-1]) return mid;
+
+            return -1;
+    }
+    int findPeakElement(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size()-1;
+        if(nums.size() == 1) return 0;
+        if(nums[0] > nums[1]) return 0;
+        if(nums[nums.size()-1] > nums[nums.size()-2]) return nums.size()-1;
+        return shiksha(l,r,nums);
     }
 };
