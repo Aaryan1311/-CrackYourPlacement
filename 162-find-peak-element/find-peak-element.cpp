5c02b21a35315aa0d314ac6cd9b46c07d1d138b1
Aaryan1311 <100665724+Aaryan1311@users.noreply.github.com>
2025-02-06 00:38:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (29.97%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    int shiksha(int l, int r, vector<int>& nums){
-            int mid = l + (r-l)/2;
-            if(mid != 0 && mid != nums.size()-1){
-                if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
-                else{
-                    if(nums[mid] < nums[mid+1] && nums[mid] > nums[mid-1]){
-                        return shiksha(mid,r,nums);
-                    }
-                    else if(nums[mid] > nums[mid+1] && nums[mid] < nums[mid-1]){
-                        return shiksha(l,mid,nums);
-                    }
-                    else{
-                        return shiksha(l,mid,nums);
-                    }
-                }
-            }
 
-            else if(mid == 0 && nums[mid+1] < nums[mid]) return mid;
-            else if(mid == nums.size()-1 && nums[mid] > nums[mid-1]) return mid;
-
-            return -1;
-    }
     int findPeakElement(vector<int>& nums) {
         int l = 0;
         int r = nums.size()-1;
         if(nums.size() == 1) return 0;
         if(nums[0] > nums[1]) return 0;
         if(nums[nums.size()-1] > nums[nums.size()-2]) return nums.size()-1;
-        return shiksha(l,r,nums);
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(mid != 0 && mid != nums.size()-1){
+                if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
+                else{
+                    if(nums[mid] < nums[mid+1] && nums[mid] > nums[mid-1]){
+                        l = mid;
+                    }
+                    else if(nums[mid] > nums[mid+1] && nums[mid] < nums[mid-1]){
+                        r = mid;
+                    }
+                    else{
+                        r = mid;
+                    }
+                }
+            }
+
+            else if(mid == 0 && nums[mid+1] < nums[mid]) return mid;
+            else if(mid == nums.size()-1 && nums[mid] > nums[mid-1]) return mid;
+        }
+        return -1;
     }
 };
