54031d6f29811d372eba99622cfeba10b3acb2c0
Aaryan1311 <100665724+Aaryan1311@users.noreply.github.com>
2025-02-14 18:05:39 +0530

    Time: 543 ms (21.90%) | Memory: 18.9 MB (96.71%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -4,24 +4,23 @@ public:
         queue<pair<string,int>> q;
         q.push({b,1});
         vector<int> vis(arr.size(),-1);
-
         while(!q.empty()){
             pair<string,int> p = q.front();
-            string w = p.first;
-            int x = p.second;
+            string s = p.first;
+            int cnt = p.second;
             for(int i = 0;i<arr.size();i++){
-                int cnt = 0;
-                for(int j = 0;j<arr[i].size();j++){
-                    if(arr[i][j] != w[j]) {
-                        cnt++;
-                    }
-                }
-                // cout << cnt << " ";
-                if(cnt == 1 && vis[i] == -1){
-                    if(arr[i] == e) return x+1;
+                if(vis[i] == -1){
                     // cout << arr[i] << endl;
-                    q.push({arr[i],x+1});
-                    vis[i] = 1;
+                    int diff = 0;
+                    for(int j = 0;j<arr[i].size();j++){
+                        if(arr[i][j] != s[j]) diff++;
+                    }
+                    if(diff == 1){
+                        vis[i] = 1;
+                        if(arr[i] == e) return cnt+1;
+                        cout << arr[i] << endl;
+                        q.push({arr[i],cnt+1});
+                    }
                 }
             }
             q.pop();
