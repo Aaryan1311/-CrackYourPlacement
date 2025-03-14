285e7092ac6bdb5fa3e040c2df1638078905e012
Aaryan1311 <100665724+Aaryan1311@users.noreply.github.com>
2025-03-15 01:42:22 +0530

    Time: 47 ms (51.99%) | Memory: 337.7 MB (46.15%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    bool check(vector<int>& nums, vector<vector<int>>& q,int mid){
+    bool check(vector<int>& nums, vector<vector<int>>& q, int k){
         int sum = 0;
-        vector<int> arr(nums.size()+1,0);
-        for(int i = 0;i<mid;i++){
-            arr[q[i][0]] += q[i][2];
-            arr[q[i][1]+1] -= q[i][2];
+        vector<int> diff(nums.size()+1,0);
+        for(int i = 0;i<k;i++){
+            diff[q[i][0]] += q[i][2];
+            diff[q[i][1]+1] -= q[i][2];
         }
         for(int i = 0;i<nums.size();i++){
-            sum += arr[i];
+            sum += diff[i];
             if(sum < nums[i]) return false;
         }
         return true;
     }
-    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& q) {
         int l = 0;
-        int r = queries.size()-1;
-        if(!check(nums,queries,queries.size())) return -1; 
+        int r = q.size()-1;
+        if(!check(nums,q,q.size())) return -1;
         while(l <= r){
             int mid = l + (r-l)/2;
-            bool pos = check(nums,queries,mid);
-            if(pos) r = mid-1;
-            else l = mid+1;
+            if(check(nums,q,mid)){
+                r = mid-1;
+            }
+            else{
+                l = mid+1;
+            }
         }
         return l;
     }
