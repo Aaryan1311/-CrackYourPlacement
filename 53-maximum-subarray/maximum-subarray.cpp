379bfad8cd3236297cf53696adb8dcaddeddb992
Aaryan1311 <100665724+Aaryan1311@users.noreply.github.com>
2025-04-11 06:30:04 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (53.33%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -5,18 +5,38 @@ public:
         int max_sum = 0;
         int max_neg = INT_MIN;
         bool flag = false;
+        int fs = -1;
+        int fe = -1;
+        int mxn = -1;
+        int s = -1;
+        int e = -1;
         for(int i = 0;i<nums.size();i++){
+            if(sum == 0){
+                s = i;
+            }
             sum += nums[i];
             if(sum < 0){
                 sum = 0;
-                max_neg = max(max_neg,nums[i]);
+                if(max_neg < nums[i]){
+                    max_neg =  nums[i];
+                    mxn = i;
+                }
             }
             else{
                 flag = true;
-                max_sum = max(max_sum,sum);
+                if(max_sum < sum){
+                    max_sum = sum;
+                    fs = s;
+                    fe = i;
+                }
             }
         }
-        if(max_sum == 0 && !flag) return max_neg;
+        
+        if(max_sum == 0 && !flag) {
+            cout << mxn << endl;
+            return max_neg;
+        }
+        cout << fs << " " << fe << endl;
         return max_sum;
     }
 };
