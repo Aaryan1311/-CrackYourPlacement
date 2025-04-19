b510c4e227f2171c215e36cb8154eacc49764e16
Aaryan1311 <100665724+Aaryan1311@users.noreply.github.com>
2025-04-19 12:09:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    // Function to find the leaders in the array.
+  public:
+    vector<int> leaders(vector<int>& arr) {
+        // Code here
+        vector<int> ans;
+        stack<int> stk;
+        for(int i = 0;i<arr.size();i++){
+            if(stk.empty()) stk.push(arr[i]);
+            else if(stk.top() < arr[i]){
+                while(!stk.empty() && stk.top() < arr[i]){
+                    stk.pop();
+                }
+                stk.push(arr[i]);
+            }
+            else{
+                stk.push(arr[i]);
+            }
+        }
+        while(!stk.empty()){
+            ans.push_back(stk.top());
+            stk.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        vector<int> result = obj.leaders(a);
+
+        // Print the result in the required format
+        if (result.empty()) {
+            cout << "[]"; // Print empty brackets if no leaders are found
+        } else {
+            for (int i = 0; i < result.size(); i++) {
+                if (i != 0)
+                    cout << " ";
+                cout << result[i];
+            }
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
