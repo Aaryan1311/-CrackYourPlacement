ec770deaa0a20903ebe239593c7550f44fbca516
Aaryan1311 <100665724+Aaryan1311@users.noreply.github.com>
2025-05-13 22:06:38 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (81.18%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -2,41 +2,12 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int sum = 0;
-        int max_sum = 0;
-        int max_neg = INT_MIN;
-        bool flag = false;
-        int fs = -1;
-        int fe = -1;
-        int mxn = -1;
-        int s = -1;
-        int e = -1;
+        int mx = INT_MIN;
         for(int i = 0;i<nums.size();i++){
-            if(sum == 0){
-                s = i;
-            }
             sum += nums[i];
-            if(sum < 0){
-                sum = 0;
-                if(max_neg < nums[i]){
-                    max_neg =  nums[i];
-                    mxn = i;
-                }
-            }
-            else{
-                flag = true;
-                if(max_sum < sum){
-                    max_sum = sum;
-                    fs = s;
-                    fe = i;
-                }
-            }
+            mx = max(sum,mx);
+            if(sum < 0) sum = 0;
         }
-        
-        if(max_sum == 0 && !flag) {
-            cout << mxn << endl;
-            return max_neg;
-        }
-        cout << fs << " " << fe << endl;
-        return max_sum;
+        return mx;
     }
 };
