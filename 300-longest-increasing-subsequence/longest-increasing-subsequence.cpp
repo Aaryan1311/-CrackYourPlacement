ac88c33facef68a2432c8e93ba4c13397e0d4445
Aaryan Tripathi <100665724+Aaryan1311@users.noreply.github.com>
2025-11-01 18:56:50 +0530

    Time: 567 ms (13.01%) | Memory: 296.6 MB (6.32%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    int helper(int idx, int prev, vector<int>& arr, vector<vector<int>> &dp){
-        if(idx == arr.size()) return 0;
-        if(dp[idx][prev+1] != -1) return dp[idx][prev+1];
-
-        int pick = 0;
-        if(prev == -1 || arr[idx] > arr[prev]){
-            pick = helper(idx+1,idx,arr,dp) + 1;
-        }
-        int notpick = helper(idx+1,prev,arr,dp);
-        return dp[idx][prev+1] = max(pick,notpick);
-    }
+int helper(int idx, int pre, vector<int>& nums, vector<vector<int>>& dp){
+    if(idx == nums.size()) return 0;
+    if(dp[idx][pre+1] != -1) return dp[idx][pre+1];
+    int choose = 0;
+    if(pre == -1 || nums[pre] < nums[idx]) choose = helper(idx+1,idx,nums,dp)+1;
+    int notchoose = helper(idx+1,pre,nums,dp);
+    return dp[idx][pre+1] = max(choose,notchoose);
+}
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> arr(nums.size(), vector<int> (nums.size()+1, -1));
-        return helper(0,-1,nums,arr);
+        vector<vector<int>> dp(nums.size()+1, vector<int> (nums.size()+1,-1));
+        return helper(0,-1,nums,dp);
     }
 };
